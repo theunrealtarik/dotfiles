2e3927d685fa231b6297f92ecb7bb88ed8a41672
tarik <tarik@me.com>
2025-10-27 02:06:29 -0400

    nvim error

--- .config/nvim/lua/plugins/core/lsp.lua
+++ .config/nvim/lua/plugins/core/lsp.lua
@@ -45,7 +45,7 @@ return {
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      require('lspconfig').gdscript.setup(capabilities)
+      -- require('lspconfig').gdscript.setup(capabilities)
 
       local servers = {
         lua_ls = {
